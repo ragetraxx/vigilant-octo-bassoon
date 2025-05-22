654eabebdd7f71ec2065a64753ffd295444063bc
ragetraxx <93855827+ragetraxx@users.noreply.github.com>
2025-05-23 00:04:15 +0800

    Update stream.py

--- stream.py
+++ stream.py
@@ -72,7 +72,7 @@ def stream_movie(movie):
             "pad=720:480:(ow-iw)/2:(oh-ih)/2[v];"
             "[1:v]scale=720:480[ol];"
             "[v][ol]overlay=0:0[vo];"
-            "[vo]drawtext=fontfile='{font}':text='{text}':fontcolor=white:fontsize=24:x=20:y=20"
+            "[vo]drawtext=fontfile='{font}':text='{text}':fontcolor=white:fontsize=10:x=20:y=20"
         ).format(font=FONT_PATH, text=text),
         "-c:v", "libx264",
         "-preset", "fast",  # better quality than 'veryfast'
